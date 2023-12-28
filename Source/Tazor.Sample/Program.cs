3168ec3e74757b9d3894e507c628c943ce6b049d
Mike Anderson <andersme87@gmail.com>
2023-12-27 17:21:56 -0800

    Add rewrite to html file

--- Source/Tazor.Sample/Program.cs
+++ Source/Tazor.Sample/Program.cs
@@ -10,6 +10,21 @@ var outputPath = Path.Combine(builder.Environment.ContentRootPath, "Output");
 Directory.CreateDirectory(outputPath);
 var fileProvider = new PhysicalFileProvider(outputPath);
 
+app.Use(async (context, func) =>
+{
+    var file = $"{context.Request.Path.Value?.TrimStart('/') ?? "index"}.html";
+    var htmlPath = Path.Combine(outputPath, file);
+    if (File.Exists(htmlPath))
+    {
+        var text = await File.ReadAllTextAsync(htmlPath);
+        await context.Response.WriteAsync(text);
+    }
+    else
+    {
+        await func.Invoke();
+    }
+});
+
 app.UseDefaultFiles(new DefaultFilesOptions
 {
     FileProvider = fileProvider
